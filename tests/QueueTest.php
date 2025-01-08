291e2edacd285c86b19ad4ed82db0096f5db823f
roiaki <roitoitoitoi@yahoo.co.jp>
2025-01-08 18:29:03 +0900

    4-19

--- tests/QueueTest.php
+++ tests/QueueTest.php
@@ -4,43 +4,57 @@ use PHPUnit\Framework\TestCase;
 
 class QueueTest extends TestCase
 {
-    protected $queue;
+    protected static $queue;
 
+    // 各テストメソッドの実行後に毎回呼び出される。
+    // 1つのテストメソッドごとにリソースのクリーンアップを行う。
     protected function setUp(): void
     {
-        $this->queue = new Queue;
+        static::$queue->clear();
     }
 
+    // 各テストメソッドの実行後に毎回呼び出される。
+    // 1つのテストメソッドごとにリソースのクリーンアップを行う。
     protected function tearDown(): void
     {
-        unset($this->queue);
+        // unset($this->queue);
+    }
+
+    // テストクラス全体が実行される前に1度だけ呼び出される。
+    public static function setUpBeforeClass(): void
+    {
+        static::$queue = new Queue;
+    }
+
+    public static function tearDownAfterClass(): void
+    {
+        static::$queue = null;
     }
 
     public function testNewQueueIsEmpty()
-    {     
-        $this->assertEquals(0, $this->queue->getCount());
+    {
+        $this->assertEquals(0, static::$queue->getCount());
     }
 
     public function testAnItemIsAddedToTheQueue()
     {
-        $this->queue->push('red');
-        $this->assertEquals(1, $this->queue->getCount());
+        static::$queue->push('red');
+        $this->assertEquals(1, static::$queue->getCount());
     }
 
     public function testAnItemIsRemovedFromTheQueue()
     {
-        $this->queue->push('red');
-        $item = $this->queue->pop();
-        $this->assertEquals(0, $this->queue->getCount());
+        static::$queue->push('red');
+        $item = static::$queue->pop();
+        $this->assertEquals(0, static::$queue->getCount());
         $this->assertEquals('red', $item);
     }
 
     public function testAnItemIsRemovedFromTheFrontOfTheQueue()
     {
-        $this->queue->push('first');
-        $this->queue->push('second');
-        
-        $this->assertEquals('first', $this->queue->pop());
+        static::$queue->push('first');
+        static::$queue->push('second');
+
+        $this->assertEquals('first', static::$queue->pop());
     }
-   
 }
